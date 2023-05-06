f91e5bdf768f2a0522e4dc557946b0cc9be8809d
jan <jan@jans-MacBook-Pro-2.local>
2023-05-06 17:18:41 +1000

    main

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,23 @@ import image from "@astrojs/image";
 import tailwind from "@astrojs/tailwind";
 import { defineConfig } from "astro/config";
 
+import partytown from "@astrojs/partytown";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://astro-moon-landing.netlify.app/",
   integrations: [
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
     tailwind(),
     image({
       serviceEntryPoint: "@astrojs/image/sharp",
     }),
+
   ],
   vite: {
     ssr: {
